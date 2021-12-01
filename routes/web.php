94d718127f3b6c415de909a4c823a64459737c31
GhifaryMN <ghifarymn1@gmail.com>
2021-12-01 18:39:59 +0700

    Belajar Upgrading Kelas 1 Des 2021

--- routes/web.php
+++ routes/web.php
@@ -57,3 +57,11 @@ Route::get('/tugas/edit/{id}','TugasController@edit');
 Route::post('/tugas/update','TugasController@update');
 
 Route::get('/tugas/hapus/{id}','TugasController@hapus');
+
+Route::get('/absen','AbsenController@index');
+Route::get('/absen/tambah','AbsenController@tambah');
+Route::post('/absen/store','AbsenController@store');
+Route::get('/absen/edit/{id}','AbsenController@edit');
+Route::post('/absen/update','AbsenController@update');
+Route::get('/absen/hapus/{id}','AbsenController@hapus');
+
